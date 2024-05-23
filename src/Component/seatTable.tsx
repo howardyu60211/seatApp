0da55427b0bcb64cb6d31e1859904f8d86f78f27
Howard <howard60211@gmail.com>
2024-05-23 13:19:47 +0800

    Add: clean seat button and disable button when seat isEmpty()

--- src/Component/seatTable.tsx
+++ src/Component/seatTable.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import {Component} from 'react'
 import {SeatButton, seatStatus} from "./seatButton";
-import {Button} from "@headlessui/react";
 
 import * as XLSX from "xlsx";
 import {saveAs} from "file-saver";
@@ -61,15 +60,9 @@ export class SeatTable extends Component<NonNullable<unknown>, SeatTableState> {
             return
         }
 
-        let changed = false
-        for (const eachStatus of this.state.seatStatusList) {
-            if (eachStatus !== seatStatus.ava) {
-                changed = true
-                break
-            }
-        }
 
-        if (changed) {
+
+        if (this.isEmpty()) {
             if (!confirm("您有尚未儲存的座位表，是否重新生成座位?")) {
                 return
             }
@@ -81,7 +74,18 @@ export class SeatTable extends Component<NonNullable<unknown>, SeatTableState> {
         console.log(this.state.c, this.state.r)
     }
 
-    changeProps = (i: number, status: seatStatus, text = "") => {
+    isEmpty () {
+        let changed = false
+        for (const eachStatus of this.state.seatStatusList) {
+            if (eachStatus !== seatStatus.ava) {
+                changed = true
+                break
+            }
+        }
+        return !changed
+    }
+
+    changeSeatStatus = (i: number, status: seatStatus, text = "") => {
         console.log("seatTable changeStatus[" + i + "] to", status, text);
         this.setState(prevState => {
             const newStatusList = [...prevState.seatStatusList];
@@ -92,6 +96,12 @@ export class SeatTable extends Component<NonNullable<unknown>, SeatTableState> {
             else newNameList[i] = text
             return {seatStatusList: newStatusList, seatNameList: newNameList};
         })
+    }
+
+    clear () {
+        for (let i=0; i<this.state.seatStatusList.length; i++) {
+            this.changeSeatStatus(i, seatStatus.ava)
+        }
     }
 
     importExcel() {
@@ -162,11 +172,11 @@ export class SeatTable extends Component<NonNullable<unknown>, SeatTableState> {
         for (const stu of students) console.log(stu)
         for (const randomIndex of shuffledSeatList) {
             if (i < students.length && this.state.seatStatusList[randomIndex] === seatStatus.ava) {
-                this.changeProps(randomIndex, seatStatus.occ, students[i])
+                this.changeSeatStatus(randomIndex, seatStatus.occ, students[i])
                 console.log("set", randomIndex, "to", students[i])
                 i++
             } else {
-                this.changeProps(randomIndex, seatStatus.emp)
+                this.changeSeatStatus(randomIndex, seatStatus.emp)
                 console.log("set", randomIndex, "to empty")
             }
         }
@@ -214,38 +224,43 @@ export class SeatTable extends Component<NonNullable<unknown>, SeatTableState> {
         for (let i = 0; i < this.state.c; i++) {
             for (let j = 0; j < this.state.r; j++) {
                 const cur = i * this.state.r + j
-                seat.push(<SeatButton key={String(cur)} id={String(cur)} draggable="true" editor={this.changeProps}
+                seat.push(<SeatButton key={String(cur)} id={String(cur)} draggable="true" editor={this.changeSeatStatus}
                                       status={this.state.seatStatusList[cur]}
                                       name={this.state.seatNameList[cur]}></SeatButton>)
             }
         }
+
         return (
             <>
                 <div className="mt-4 w-full px-6 flex flex-wrap">
                     {/* seat card */}
                     <div className="w-full max-w-full pb-1 pl-0">
-                        <div className="flex flex-wrap -mx-3 pb-2">
+                        <div className="flex flex-row -mx-3 pb-2">
                             {/* text */}
-                            <div className="flex flex-col flex-none w-1/2 max-w-full px-3">
+                            <div className="flex flex-col flex-none w-fit px-3">
                                 <p className="select-none text-[#EDF0F4] font-medium text-[14px] leading-[130%] pb-[8px]">學生座位預覽</p>
                                 <p className="select-none text-[#ACB4C0] font-normal text-[14px] leading-[130%]">在此修改並匯出學生座位。</p>
                             </div>
 
                             {/* Button */}
-                            <div className="flex flex-row-reverse w-1/2 max-w-full px-3 text-right">
+                            <div className="flex flex-row-reverse w-full px-3 text-right">
                                 <button
-                                    className="select-none h-11 px-4 mx-1 mb-0 font-bold text-center uppercase align-middle transition-all bg-transparent border border-solid rounded-lg shadow-none cursor-pointer leading-pro ease-soft-in text-xs duration-100 bg-150 active:opacity-85 hover:scale-102 tracking-tight-soft bg-x-25 border-opacity-0 hover:border-opacity-100 border-fuchsia-500 text-fuchsia-500"
+                                    className="select-none h-11 px-4 ml-1 mb-0 font-bold text-center uppercase align-middle transition-all bg-transparent border border-solid rounded-lg shadow-none cursor-pointer leading-pro ease-soft-in text-xs duration-100 bg-150 active:opacity-85 hover:scale-102 tracking-tight-soft bg-x-25 border-opacity-0 hover:border-opacity-100 border-fuchsia-500 text-fuchsia-500"
                                     onClick={this.importExcel}>匯入學生
                                 </button>
                                 <button
                                     className="select-none h-11 px-4 mx-1 mb-0 font-bold text-center uppercase align-middle transition-all bg-transparent border border-solid rounded-lg shadow-none cursor-pointer leading-pro ease-soft-in text-xs duration-100 bg-150 active:opacity-85 hover:scale-102 tracking-tight-soft bg-x-25 border-opacity-0 hover:border-opacity-100 border-fuchsia-500 text-fuchsia-500"
                                     onClick={() => {
                                         document.getElementById("openAskDialog").click()
-                                    }}>生成新座位
+                                    }}>生成座位
                                 </button>
-                                <Button id="secondGeneration" className="Hidden" onClick={this.resizeSeat}></Button>
-                                <button onClick={this.exportExcel}
-                                        className="select-none h-11 px-4 mx-1 mb-0 font-bold text-center uppercase align-middle transition-all bg-transparent border border-solid rounded-lg shadow-none cursor-pointer leading-pro ease-soft-in text-xs duration-100 bg-150 active:opacity-85 hover:scale-102 tracking-tight-soft bg-x-25 border-opacity-0 hover:border-opacity-100 border-fuchsia-500 text-fuchsia-500">匯出座位
+                                <button
+                                    className="select-none h-11 px-4 mx-1 mb-0 font-bold text-center uppercase align-middle transition-all bg-transparent border border-solid rounded-lg shadow-none cursor-pointer leading-pro ease-soft-in text-xs duration-100 bg-150 active:opacity-85 hover:scale-102 tracking-tight-soft bg-x-25 border-opacity-0 hover:border-opacity-100 border-fuchsia-500 text-fuchsia-500 disabled:border-opacity-0 disabled:text-gray-700"
+                                    onClick={() => {this.clear()}} disabled={this.isEmpty()}>清空座位
+                                </button>
+                                <button id="secondGeneration" className="Hidden" onClick={this.resizeSeat}></button>
+                                <button onClick={this.exportExcel} disabled={this.isEmpty()}
+                                        className="select-none h-11 px-4 mx-1 mb-0 font-bold text-center uppercase align-middle transition-all bg-transparent border border-solid rounded-lg shadow-none cursor-pointer leading-pro ease-soft-in text-xs duration-100 bg-150 active:opacity-85 hover:scale-102 tracking-tight-soft bg-x-25 border-opacity-0 hover:border-opacity-100 border-fuchsia-500 text-fuchsia-500 disabled:border-opacity-0 disabled:text-gray-700">匯出座位
                                 </button>
                             </div>
                         </div>
@@ -256,9 +271,14 @@ export class SeatTable extends Component<NonNullable<unknown>, SeatTableState> {
                         {seat}
                     </div>
                     <StatusBar
-                        blueNum={this.state.seatStatusList.filter((x) => {return x == seatStatus.occ}).length}
-                        greenNum={this.state.seatStatusList.filter((x) => {return x == seatStatus.ava}).length}
-                        redNum={this.state.seatStatusList.filter((x) => {return x == seatStatus.emp}).length}
+                        blueNum={this.state.seatStatusList.filter((x) => {
+                            return x == seatStatus.occ
+                        }).length}
+                        greenNum={this.state.seatStatusList.filter((x) => {
+                            return x == seatStatus.ava
+                        }).length}
+                        redNum={this.state.seatStatusList.filter((x) => {
+                            return x == seatStatus.emp}).length}
                             >
                             </StatusBar>
                 </div>
